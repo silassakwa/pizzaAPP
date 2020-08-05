c34f3baa93c23405c4f29808d2fda1af2543064f
silassakwa <silassakwa2016@gmail.com>
2020-08-05 15:18:53 +0300

    modified computation for order

--- js/jscript.js
+++ js/jscript.js
@@ -1,4 +1,4 @@
-function Pizza(flavor ,toppings,Crust,size, quantity, price) {
+function Pizza(flavor ,toppings,crust,size, quantity, price) {
   this.flavor = flavor;
   this.toppings = toppings;
   this.crust=crust;
@@ -9,9 +9,9 @@ function Pizza(flavor ,toppings,Crust,size, quantity, price) {
 
 var small=550;
 var medium=830;
-var large=1030;
-var topping =100;
-var crust=50;
+var large=1050;
+var topping=75;
+
 
 
 $(document).ready(function() {
@@ -25,26 +25,35 @@ $("#order").click(
     quantity=parseInt($('#quantity').val());
 
     // Getting the total amount
-    if (size == "small" && toppings == ""&& crust==""){
+    if (size == "small" && toppings == "none"){
       var price = small * quantity;
     }
-    else if (size == "small" && toppings == "tomato" || toppings == " pineapple"&& crust=="cripsy"||crust=="stuffed"||crust=="gluten-free"){
-      var price = small * quantity + topping+crust;
-    }if(size == "medium" && toppings =="" && crust==""){
-      var price = medium * quantity;
-    }else if(size == "medium" && toppings == "tomato" || toppings == " pineapple"&& crust=="cripsy"||crust=="stuffed"||crust=="gluten-free"){
-      var price = medium * quantity + topping+crust;
-    }if (size == "large" && toppings == ""&& crust==""){
-      var price = large * quantity;
-    }else if (size == "large" && toppings ==" tomato" || toppings == " pineapple"&& crust=="cripsy"||crust=="stuffed"||crust=="gluten-free") {
-      var price= large * quantity + topping+crust;
+    else if (size == "small" && toppings == "t" || toppings == "p"){
+      var price = small * quantity + topping;
     }
+    else if (size == "medium" && toppings == "none"){
+      var price = medium * quantity;
+    }
+    else if (size == "medium" && toppings == "t" || toppings == "p"){
+      var price = medium * quantity + topping;
+    }
+    if (size == "large" && toppings == "none"){
+      var price = large * quantity;
+    }
+    else if (size == "large" && toppings == "t" || toppings == "p"){
+      var price = large * quantity + topping;
 
+    }
+    
+    
+    
+
+    
+  
     var newPizza = new Pizza(flavor,toppings,crust,size,quantity,price);
     var newRow = '<tr><td id="flav">' + newPizza.flavor  + '</td><td id="toppings">'+$("#toppings Option:selected").text()+'</td><td id="crust">'+$("#crust option:selected").text()+'</td><td id="size">'+$("#size option:selected").text()+'</td><td is="quantinty">'+$("#quantity option:selected").text()+'</td><td id="total">'+newPizza.price+'</tr>'
-$("#pizza").append(newRow);
-}
-);
+    $("#pizza").append(newRow);
+      });
 $("#deliver").click(function(){
   alert("Delivary Fee is ksh 200 within Nairobi and ksh 400 outside Nairobi")
   prompt("Enter location for delivery");
