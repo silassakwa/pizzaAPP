1cd113dc26fb6db460d891cda460233f95434f85
silassakwa <silassakwa2016@gmail.com>
2020-08-05 12:23:48 +0300

    modified computation functionality of js file

--- js/jscript.js
+++ js/jscript.js
@@ -1,6 +1,7 @@
-function Pizza(flavor ,toppings,size, quantity, price) {
+function Pizza(flavor ,toppings,Crust,size, quantity, price) {
   this.flavor = flavor;
   this.toppings = toppings;
+  this.crust=crust;
   this.size = size;
   this.quantity = quantity;
   this.price = price;
@@ -10,6 +11,7 @@ var small=550;
 var medium=830;
 var large=1030;
 var topping =100;
+var crust=50;
 
 
 $(document).ready(function() {
@@ -23,32 +25,30 @@ $("#order").click(
     quantity=parseInt($('#quantity').val());
 
     // Getting the total amount
-    if (size == "small" && toppings == ""){
+    if (size == "small" && toppings == ""&& crust==""){
       var price = small * quantity;
     }
-    else if (size == "small" && toppings == "Extra tomato" || toppings == "Extra pineapple"){
-      var price = small * quantity + topping;
-    }if(size == "medium" && toppings =="" ){
+    else if (size == "small" && toppings == "tomato" || toppings == " pineapple"&& crust=="cripsy"||crust=="stuffed"||crust=="gluten-free"){
+      var price = small * quantity + topping+crust;
+    }if(size == "medium" && toppings =="" && crust==""){
       var price = medium * quantity;
-    }else if(size == "medium" && toppings == "Extra tomato" || toppings == "Extra pineapple"){
-      var price = medium * quantity + topping;
-    }if (size == "large" && toppings == ""){
+    }else if(size == "medium" && toppings == "tomato" || toppings == " pineapple"&& crust=="cripsy"||crust=="stuffed"||crust=="gluten-free"){
+      var price = medium * quantity + topping+crust;
+    }if (size == "large" && toppings == ""&& crust==""){
       var price = large * quantity;
-    }else if (size == "large" && toppings =="Extra tomato" || toppings == "Extra pineapple") {
-      var price= large * quantity + topping;
+    }else if (size == "large" && toppings ==" tomato" || toppings == " pineapple"&& crust=="cripsy"||crust=="stuffed"||crust=="gluten-free") {
+      var price= large * quantity + topping+crust;
     }
 
-    var newPizza = new Pizza(flavor,toppings,size,quantity,price);
-    var newRow = '<tr><th id="flav">' + newPizza.flavor + '</th><td id="tp">' + $("#toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="sz">' + $("#size option:selected").text() + " - " + newPizza.size + '</td><td id="bxs">' + $("#quantity option:selected").text() + " - " + newPizza.quantity + '</td><td id="total">' + newPizza.price + '</td></tr>'
+    var newPizza = new Pizza(flavor,toppings,crust,size,quantity,price);
+    var newRow = '<tr><td id="flav">' + newPizza.flavor  + '</td><td id="toppings">'+$("#toppings Option:selected").text()+'</td><td id="crust">'+$("#crust option:selected").text()+'</td><td id="size">'+$("#size option:selected").text()+'</td><td is="quantinty">'+$("#quantity option:selected").text()+'</td><td id="total">'+newPizza.price+'</tr>'
 $("#pizza").append(newRow);
-
-
-
-
-  }
+}
 );
 $("#deliver").click(function(){
+  alert("Delivary Fee is ksh 200 within Nairobi and ksh 400 outside Nairobi")
   prompt("Enter location for delivery");
+  
 });
 
 
